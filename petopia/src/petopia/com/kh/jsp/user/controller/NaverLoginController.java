1125d6720235ee4dce3b5f1ddf16c9e90df8d0d1
sibaLL <129156800+sibaLL@users.noreply.github.com>
2023-10-06 10:57:19 +0900

    sql

--- petopia/src/petopia/com/kh/jsp/user/controller/NaverLoginController.java
+++ petopia/src/petopia/com/kh/jsp/user/controller/NaverLoginController.java
@@ -1,6 +1,12 @@
 package petopia.com.kh.jsp.user.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -27,7 +33,49 @@ public class NaverLoginController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		response.getWriter().print(request.getParameter("mobile"));
+		
+		//System.out.println(request.getParameter("code"));
+		//System.out.println(request.getParameter("state"));
+		String clientId = "8ZYnqypIAIHZc2Ycz4px";
+		String clientSecret = "wd7mxCBxd2";
+		String code = request.getParameter("code");
+		String state = request.getParameter("state");
+		String redirectURI = URLEncoder.encode("http://localhost:8001/petopia/naverLogin","UTF-8");
+		String apiURL;
+		apiURL = "https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&";
+	    apiURL += "client_id=" + clientId;
+	    apiURL += "&client_secret=" + clientSecret;
+	    apiURL += "&redirect_uri=" + redirectURI;
+	    apiURL += "&code=" + code;
+	    apiURL += "&state=" + state;
+	    String access_token = "";
+	    String refresh_token = "";
+	    System.out.println("apiURL="+apiURL);
+	    
+	    try {
+	    	URL url = new URL(apiURL);
+	    	HttpURLConnection con = (HttpURLConnection)url.openConnection();
+	    	con.setRequestMethod("GET");
+	    	int responseCode = con.getResponseCode();
+	    	BufferedReader br;
+	    	System.out.println("responseCode="+responseCode);
+	    	if(responseCode==200) { // 정상 호출
+	            br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+	          } else {  // 에러 발생
+	            br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
+	          }
+	          String inputLine;
+	          StringBuffer res = new StringBuffer();
+	          while ((inputLine = br.readLine()) != null) {
+	            res.append(inputLine);
+	          }
+	          br.close();
+	          if(responseCode==200) {
+	            System.out.println(res.toString());
+	          }
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
